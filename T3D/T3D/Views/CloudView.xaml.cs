4e9b1853d4563155f96bd557395ba714a9ab0f91
reganpu <reganpu@gmail.com>
2017-04-26 11:15:49 +0800

    v0.1.1 Hard Coding Wrapped in Functions

--- T3D/T3D/Views/CloudView.xaml.cs
+++ T3D/T3D/Views/CloudView.xaml.cs
@@ -15,27 +15,29 @@ namespace T3D
 		string fileExtension = ".txt";
 		string imageExtension = ".png";
 
+		string[] coverImagePath;
+		List<byte[]> coverImageAsBytesList = new List<byte[]>();
+		ObservableCollection<ItemInTheCloud> list = new ObservableCollection<ItemInTheCloud>();
+
+		string[] sliceNumber = { "001", "002", "003", "004", "005"};
+		string[] sliceImagePath;
+		// save slice images to files as byte array 
+		List<byte[]> imageAsBytesList = new List<byte[]>();
+
 		public CloudView()
 		{
 			InitializeComponent();
 
-			string[] path = { bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + modelNumber + imageExtension,
-						  	  bluehostDNS + "/" + fileDirectory + "/" + fileName[1] + "_/" + fileName[1] + "_" + modelNumber + imageExtension,
-						  	  bluehostDNS + "/" + fileDirectory + "/" + fileName[2] + "_/" + fileName[2] + "_" + modelNumber + imageExtension};
+			coverImagePath= new string[] { bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + modelNumber + imageExtension,
+						  	  	  		   bluehostDNS + "/" + fileDirectory + "/" + fileName[1] + "_/" + fileName[1] + "_" + modelNumber + imageExtension,
+						  	  	  		   bluehostDNS + "/" + fileDirectory + "/" + fileName[2] + "_/" + fileName[2] + "_" + modelNumber + imageExtension
+										 };
 			// Connected to the tested server in T3-106
 			//string path = "http://140.118.198.70:8080/walk3dprinter/slice/OwlStatue_/OwlStatue_000.png";
-			List<byte[]> imageAsBytesList = new List<byte[]>();
+
 			//
-			ObservableCollection<ItemInTheCloud> list = new ObservableCollection<ItemInTheCloud>();
 			listView.ItemsSource = list;
-			for (int i = 0; i < fileName.Length; i++)
-			{
-				imageAsBytesList.Add(DependencyService.Get<ISaveAndLoad>().GetAByteImageFromWeb(path[i]));
-				DependencyService.Get<ISaveAndLoad>().SaveByteImage(fileName[i], modelNumber, fileExtension, imageAsBytesList[i]);
-
-				list.Add(new ItemInTheCloud() { ImageFilePath = path[i], Name = fileName[i], Notes = notes[i] });
-			}
-
+			downloadImages();
 			//BindingContext = new ItemInTheCloud()
 			//{
 			//	ImageFilePath = path,
@@ -44,35 +46,50 @@ namespace T3D
 			//};
 
 			//image.Source = ImageSource.FromStream(() => new MemoryStream(imageAsBytes));
+		}
 
+		void downloadImages()
+		{
+			for (int i = 0; i<fileName.Length; i++)
+			{
+				coverImageAsBytesList.Add(DependencyService.Get<ISaveAndLoad>().GetAByteImageFromWeb(coverImagePath[i]));
+				DependencyService.Get<ISaveAndLoad>().SaveByteImage(fileName[i], modelNumber, fileExtension, coverImageAsBytesList[i]);
 
+				list.Add(new ItemInTheCloud() { ImageFilePath = coverImagePath[i], Name = fileName[i], Notes = notes[i] });
+			}
 		}
 
 	    void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
 		{
 			ItemInTheCloud itemInTheCloud = (ItemInTheCloud)e.SelectedItem;
-			string[] sliceNumber = { "001", "002", "003", "004", "005"};
+
 			if (itemInTheCloud.Name == fileName[0])
 			{
-				string[] path = { bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[0] + imageExtension,
-								  bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[1] + imageExtension,
-								  bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[2] + imageExtension,
-								  bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[3] + imageExtension,
-								  bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[4] + imageExtension			  
-								};
-				// save slice images to files as byte array 
-				List<byte[]> imageAsBytesList = new List<byte[]>();
-				for (int i = 0; i < sliceNumber.Length; i++)
-				{
-				    imageAsBytesList.Add(DependencyService.Get<ISaveAndLoad>().GetAByteImageFromWeb(path[i]));
-					DependencyService.Get<ISaveAndLoad>().SaveByteImage(fileName[0], sliceNumber[i], fileExtension, imageAsBytesList[i]);
-				}
+				sliceImagePath = new string[] { bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[0] + imageExtension,
+								  				bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[1] + imageExtension,
+								  				bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[2] + imageExtension,
+								  				bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[3] + imageExtension,
+								  				bluehostDNS + "/" + fileDirectory + "/" + fileName[0] + "_/" + fileName[0] + "_" + sliceNumber[4] + imageExtension			  
+											  };
 
-				// inform storage to update (Event)
+				downloadSlicesOfSelectedModel();
+
+				/// inform storage to update (Event)
 
 			}
+
 			// test
 			//Navigation.PushAsync(new Page());
 		}
+
+		void downloadSlicesOfSelectedModel()
+		{
+			for (int i = 0; i<sliceNumber.Length; i++)
+				{
+				    imageAsBytesList.Add(DependencyService.Get<ISaveAndLoad>().GetAByteImageFromWeb(sliceImagePath[i]));
+					DependencyService.Get<ISaveAndLoad>().SaveByteImage(fileName[0], sliceNumber[i], fileExtension, imageAsBytesList[i]);
+				}
+		}
+
 	}
 }
